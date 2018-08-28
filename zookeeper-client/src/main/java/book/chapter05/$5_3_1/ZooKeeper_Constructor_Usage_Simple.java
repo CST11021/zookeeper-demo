fea27a321526945d7a907a05f163a6c465952d70
wanghz <wanghz@tuya.com>
2018-08-28 16:41:19 +0800

    添加zookeeper源码

---
+++ zookeeper-client/src/main/java/book/chapter05/$5_3_1/ZooKeeper_Constructor_Usage_Simple.java
@@ -0,0 +1,60 @@
+package book.chapter05.$5_3_1;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooKeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Java API -> 创建连接 -> 创建一个最基本的ZooKeeper对象实例
+ */
+public class ZooKeeper_Constructor_Usage_Simple implements Watcher {
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws Exception{
+
+        ZooKeeper zookeeper = new ZooKeeper(
+                "localhost:2181",
+                5000,
+                new ZooKeeper_Constructor_Usage_Simple());
+        System.out.println(zookeeper.getState());
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ZooKeeper session established.");
+    }
+
+    /**
+     * 在收到服务端发来的SyncConnected事件之后，解除主程序CountDownLatch上的等待阻塞。
+     *
+     * 注意，ZooKeeper客户端和服务端会话的建立是一个异步的过程，也就是说在程序中，构造方法会在处理完客户端初始化工作后立
+     * 即返回，在大多数情况下，此时并没有真正建立好一个可用的会话，在会话的生命周期中处于“CONNECTION”的状态。
+     *
+     * 当该会话真正创建完毕后，ZooKeeper服务端会向会话对应的客户端发送一个事件通知，以告知客户端，客户端只有在获取这个通
+     * 知之后，才算真正建立了会话。
+     *
+     * 该构造方法内部实现了与ZooKeeper服务器之间的TCP连接创建，负责维护客户端会话的生命周期。
+     *
+     * @param event
+     */
+    @Override
+    public void process(WatchedEvent event) {
+        System.out.println("Receive watched event：" + event);
+        if (Event.KeeperState.SyncConnected == event.getState()) {
+            connectedSemaphore.countDown();
+            System.out.println("create session success!");
+        }
+    }
+
+}
